fd6653efbe1a26b5b4d5a391c5ddda275faaab8b
kk1522 <102128569+kk1522@users.noreply.github.com>
2022-03-28 08:34:43 +0530

    Add files via upload

--- feedbackform.java
+++ feedbackform.java
@@ -1,5 +1,12 @@
 package packagetest.restaurant;
 
+import packagetest.restaurant.customer.Customer;
+
 public class feedbackform {
+	
+	String review;
+	protected int ratingOutOfFive;
+	Customer customer = new Customer(null, null);
+	
 
 }
